75197464192ac6647e5673475820594d7fe191e9
Rohit Amarnath <rohit.amarnath@full360.com>
2017-07-20 16:30:40 -0400

    fix ecr command switch sequence

--- docker-ci.mk
+++ docker-ci.mk
@@ -32,7 +32,7 @@ SHELL := bash
 .DELETE_ON_ERROR:
 .SUFFIXES:
 
-ECR_GET_LOGIN := aws ecr get-login --registry-ids --no-include-email
+ECR_GET_LOGIN := aws ecr get-login --no-include-email --registry-ids 
 
 REVISION = $(shell git rev-parse --short HEAD)
 
